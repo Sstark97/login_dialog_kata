9e9ef5795397b939aab722afe4eb178761c8c143
Sstark97 <aitorscinfo@gmail.com>
2024-05-04 10:17:25 +0100

    :sparkles: Test LoginDialog with Mockito

--- src/test/java/dev/ascinfo/LoginDialogTest.java
+++ src/test/java/dev/ascinfo/LoginDialogTest.java
@@ -1,12 +1,50 @@
 package dev.ascinfo;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class LoginDialogTest {
   @Test
-  void have_fun_using_test_doubles() {
-    assertTrue(true);
+  void correctly_invokes_authenticator() {
+    Authenticator authenticatorMock = Mockito.mock(Authenticator.class);
+    LoginDialog dialog = new LoginDialog(authenticatorMock);
+
+    when(authenticatorMock.authenticate("user", "pw")).thenReturn(true);
+    dialog.show();
+    boolean success = dialog.submit("user", "pw");
+
+    assertTrue(success);
+    verify(authenticatorMock, times(1)).authenticate("user", "pw");
+  }
+
+  @Test
+  void fail_invokes_authenticator_with_bad_credentials() {
+    Authenticator authenticatorMock = Mockito.mock(Authenticator.class);
+    LoginDialog dialog = new LoginDialog(authenticatorMock);
+
+    when(authenticatorMock.authenticate("user", "pw")).thenReturn(false);
+    dialog.show();
+    boolean success = dialog.submit("user", "pw");
+
+    assertFalse(success);
+    verify(authenticatorMock, times(1)).authenticate("user", "pw");
+  }
+
+  @Test
+  void not_call_authenticator_when_is_closed() {
+    Authenticator authenticatorMock = Mockito.mock(Authenticator.class);
+    LoginDialog dialog = new LoginDialog(authenticatorMock);
+
+    dialog.show();
+    dialog.close();
+
+    assertFalse(dialog.isOpen());
+    verify(authenticatorMock, times(0)).authenticate(anyString(), anyString());
   }
 }

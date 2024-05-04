06f33719f1ac8959f354e32ff978562ae8017a40
Sstark97 <aitorscinfo@gmail.com>
2024-05-04 08:55:16 +0100

    :recycle: Add test class

---
+++ src/test/java/dev/ascinfo/LoginDialogTest.java
@@ -0,0 +1,12 @@
+package dev.ascinfo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginDialogTest {
+  @Test
+  void have_fun_using_test_doubles() {
+    assertTrue(true);
+  }
+}
